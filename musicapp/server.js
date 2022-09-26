1ec2657485d7a0984fdf61654f94394428876b51
Parth More <parthmore0116@gmail.com>
2022-09-26 19:26:31 -0400

    React,Express,SQL all connected

--- musicapp/server.js
+++ musicapp/server.js
@@ -1,9 +1,58 @@
+/*
+const express = require("express");
+const { spawn } = require("child_process");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+var mysql = require("mysql");
+
+const app = express();
+
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Cheese1601",
+  database: "support",
+});
+
+//connect to database
+con.connect((err) => {
+  if (err) throw err;
+  console.log("Mysql Connected...");
+});
+
+
+
+app.listen(3001, () => {
+  console.log("Server running successfully on 3001");
+});
+
+*/
+
+
 const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 5001; //Line 3
+var mysql = require("mysql");
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Ilovedatabases",
+    database: "music",
+});
+
+//connect to database
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Mysql Connected...");
+});
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
