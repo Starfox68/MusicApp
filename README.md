61085dd3c6cdfad996d9b410770d8eb1a39558fe
Henry <henryyu0198@gmail.com>
2022-09-26 21:02:45 -0400

    Add sample database, update readme

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Start a MySQL server on your local environment with username "localhost", password "Ilovedatabases", and database name "music"
+
+Load the sample database: `mysql -u localhost -p Ilovedatabases < sampleDatabase.sql`
+`
+
---
+++ sampleDatabase.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SAMPLE (
+    ID INT(10) NOT NULL PRIMARY KEY
+    songName VARCHAR(255)
+    artist VARCHAR(255)
+    genre VARCHAR(255)
+);
+
+INSERT INTO SAMPLE VALUES (1, "Despacito", "Luis Fonsi", "Cringe");
+INSERT INTO SAMPLE VALUES (2, "Fornite", "Lorem Ipsum", "123");
+INSERT INTO SAMPLE VALUES (3, "abc", "Among Us", ":)");
+
