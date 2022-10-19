0cf8aee006b3751cdc45e37bea0c2b40390a5d9e
Frank Chen <frkchen1@gmail.com>
2022-10-19 17:03:38 -0400

    add sample data

--- loadSampleDatabase.sql
+++ loadSampleDatabase.sql
@@ -2,7 +2,10 @@ INSERT INTO Song VALUES (1, "Despacito", "google.ca", "2017-1-13", "despacito!")
 INSERT INTO Artist VALUES(1,  "Luis Fonsi");
 INSERT INTO SongAuthor VALUES(1, 1);
 
-INSERT INTO User VALUES ("user1", "password1")
-INSERT INTO User VALUES ("user2", "password2")
+INSERT INTO User VALUES ("user1", "password1");
+INSERT INTO User VALUES ("user2", "password2");
 
-INSERT INTO Playlist VALUES ("user1", 1, "playlistname")
+INSERT INTO Playlist VALUES ("user1", 1, "coolsongs", "2022-10-19");
+
+INSERT INTO PlaylistSong VALUES (1, 1, "user1");
+
